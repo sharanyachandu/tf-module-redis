0de4b99b990ffbcc649de730ddf9d80172014bbd
sharanya chandu <you@example.com>
2023-06-29 00:35:25 +0530

    auto save

--- data.tf
+++ data.tf
@@ -2,7 +2,7 @@
 data "terraform_remote_state" "vpc" {
   backend = "s3"
   config = {
-    bucket = "b53-tfstate-bucket"
+    bucket = "b53-s3-bucket"
     key    = "vpc/${var.ENV}/terraform.tfstate"
     region = "us-east-1"
    }
